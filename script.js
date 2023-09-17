fe6a9a343a816f46aee39228af6b71d2fe6d4ab9
PhillipMagos <phillipmagos@gmail.com>
2023-09-17 01:18:22 -0500

    1st push

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// function animal() {
+//   const x = prompt("Enter your favorite animal!");
+//   document.write("Your favorite animal is " + x );
+// }
+// animal();
+
+// function cities(){
+//   console.log("San Diego");
+// }
+// function cities(){
+//   console.log("San Diego");
+// }
+
+// function cities() {
+//   console.log("San Diego");
+// }
+
+// function cities() {
+//   console.log("San Diego");
+// }
+
+// function cities(){
+//   console.log("San Diego")
+// }
+
+// function favBook() {
+  
+// }
